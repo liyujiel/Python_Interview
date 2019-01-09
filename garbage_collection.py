9d6eeda0574a8d41e57124d4f25b78774d72c077
Alex Li <li1995717@gmail.com>
2019-01-09 14:58:33 +0800

    update gc example

---
+++ garbage_collection.py
@@ -0,0 +1,35 @@
+import gc
+
+
+counter = 0
+
+class SingleThreadObject(object):
+    def __init__(self):
+        global counter
+        mark = counter
+        a = list()
+        for _ in range(10):
+            a = [a]
+            a.append(a)
+
+        del a
+        counter = mark + 1
+
+    def __del__(self):
+        global counter
+        mark = counter
+        counter = mark - 1
+
+
+for _ in range(10000):
+    a = list()
+    b = list()
+    a.append(b)
+    b.append(a)
+    a.append(SingleThreadObject())
+    del a
+    del b
+    gc.collect() # if not comments, large number will shows up
+
+# gc.collect()
+print(counter)
