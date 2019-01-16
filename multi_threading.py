557801572a3581331a9fb4a25cbfef392e092fbd
Alex Li <li1995717@gmail.com>
2019-01-16 18:09:10 +0800

    add threading example

--- multi_threading.py
+++ multi_threading.py
@@ -1,1 +1,31 @@
+import threading
+import time
+
+
+START = "start"
+END = "end"
+
+
+def work(event, index):
+    print(START + ": " + str(index))
+    event.wait()
+    print(END)
+
+
+def threading_main():
+    event = threading.Event()
+    for i in range(5):
+        t = threading.Thread(target=work, args=(event, i))
+        t.start()
+
+    # stop all thread
+    event.clear()
+    start_flag = input("Want end thread?")
+    # print(start_flag)
+    if start_flag:
+        event.set()
+
+
+if __name__ == '__main__':
+    threading_main()
 
